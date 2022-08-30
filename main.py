aadbf321a963bbc54acc2e64eaf2242c957b488e
aizimpamvu <hallyiizza200@gmail.com>
2022-08-30 23:59:31 +0200

    add get coordinate on click function

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import pandas
+import turtle
 
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+
+screen.addshape(image)
+
+turtle.shape(image)
 data = pandas.read_csv("50_states.csv")
 data_dict = data.to_dict()
 print(data_dict)
+
+
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_mouse_click_coor)
+
+turtle.mainloop()
+
+# screen.exitonclick()
